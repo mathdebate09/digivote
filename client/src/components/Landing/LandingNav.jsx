9840bbd45bb9c00a31ad1c7b52ff6e6b6456c13d
Jay Singh <140599484+mathdebate09@users.noreply.github.com>
2024-09-27 22:42:21 +0530

    chore: landing responsive

--- client/src/components/Landing/LandingNav.jsx
+++ client/src/components/Landing/LandingNav.jsx
@@ -6,15 +6,14 @@ import logo from "../../assets/logos/digivote.png";
 
 function LandingNav({}) {
   return (
-    <nav className="flex items-center justify-between bg-transparent px-24 py-8 font-body font-medium">
-      <div className="flex items-center gap-4 text-3xl font-bold text-black-soft">
+    <nav className="flex items-center justify-start md:justify-between bg-transparent md:px-24 md:py-8 px-4 py-6 font-body font-medium">
+      <Link to="\" className="flex items-center gap-4 select-none text-3xl font-bold text-black-soft">
         <img className="w-16" src={logo}></img>
         <h1 className="font-logo text-blue-950">DigiVote</h1>
-      </div>
-      <ul className="flex items-center gap-4">
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/contact">Contact</Link>
+      </Link>
+      <ul className="md:flex items-center gap-4 hidden">
+        <Link to="/home">Home</Link>
+        <Link to="/boothmap">Booths</Link>
         <button className="rounded-lg bg-blue-600 px-4 py-2 font-bold text-white">
           Sign Up
         </button>
