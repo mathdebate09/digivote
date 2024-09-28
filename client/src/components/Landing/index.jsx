0cd4eb9a520dd81baf8991f48fa4fbedfa3cd1b7
Jay Singh <140599484+mathdebate09@users.noreply.github.com>
2024-09-28 11:58:25 +0530

    chore: gsap init

--- client/src/components/Landing/index.jsx
+++ client/src/components/Landing/index.jsx
@@ -11,67 +11,67 @@ import Hero from "./Hero";
 import LandingNav from "./LandingNav";
 
 function Landing() {
-  const textVariants = {
-    hidden: { opacity: 0, x: -50 },
-    visible: { opacity: 1, x: 0, transition: { duration: 1 } },
-  };
+  // const textVariants = {
+  //   hidden: { opacity: 0, x: -50 },
+  //   visible: { opacity: 1, x: 0, transition: { duration: 1 } },
+  // };
 
-  useEffect(() => {
-    // Initial setup for the purple ball animation
-    gsap.set(".ball", { xPercent: -50, yPercent: -50 });
-    let targets = gsap.utils.toArray(".ball");
+  // useEffect(() => {
+  //   // Initial setup for the purple ball animation
+  //   gsap.set(".ball", { xPercent: -50, yPercent: -50 });
+  //   let targets = gsap.utils.toArray(".ball");
 
-    window.addEventListener("mouseleave", () => {
-      gsap.to(targets, {
-        duration: 0.5,
-        scale: 0,
-        ease: "power1.out",
-        overwrite: "auto",
-        stagger: 0.02,
-      });
-    });
+  //   window.addEventListener("mouseleave", () => {
+  //     gsap.to(targets, {
+  //       duration: 0.5,
+  //       scale: 0,
+  //       ease: "power1.out",
+  //       overwrite: "auto",
+  //       stagger: 0.02,
+  //     });
+  //   });
 
-    window.addEventListener("mouseenter", () => {
-      gsap.to(targets, {
-        duration: 0.5,
-        scale: 1,
-        ease: "power1.out",
-        overwrite: "auto",
-        stagger: 0.02,
-      });
-    });
+  //   window.addEventListener("mouseenter", () => {
+  //     gsap.to(targets, {
+  //       duration: 0.5,
+  //       scale: 1,
+  //       ease: "power1.out",
+  //       overwrite: "auto",
+  //       stagger: 0.02,
+  //     });
+  //   });
 
-    window.addEventListener("mousemove", (e) => {
-      gsap.to(targets, {
-        duration: 0.5,
-        x: e.clientX,
-        y: e.clientY,
-        ease: "power1.out",
-        overwrite: "auto",
-        stagger: 0.02,
-      });
-    });
+  //   window.addEventListener("mousemove", (e) => {
+  //     gsap.to(targets, {
+  //       duration: 0.5,
+  //       x: e.clientX,
+  //       y: e.clientY,
+  //       ease: "power1.out",
+  //       overwrite: "auto",
+  //       stagger: 0.02,
+  //     });
+  //   });
 
-    // Cleanup event listeners on component unmount
-    return () => {
-      window.removeEventListener("mouseleave", () => {});
-      window.removeEventListener("mouseenter", () => {});
-      window.removeEventListener("mousemove", () => {});
-    };
-  }, []);
+  //   // Cleanup event listeners on component unmount
+  //   return () => {
+  //     window.removeEventListener("mouseleave", () => {});
+  //     window.removeEventListener("mouseenter", () => {});
+  //     window.removeEventListener("mousemove", () => {});
+  //   };
+  // }, []);
   return (
-    <div className="bg-gradient-to-tr from-gray-100 via-cyan-50 to-blue-200">
-      <motion.div
+    <div className="bg-gradient-to-tr from-gray-100 via-cyan-50 to-blue-200 bg-opacity-5">
+      {/* <motion.div
         className="text-center text-8xl font-bold max-md:text-6xl max-sm:text-4xl"
         variants={textVariants}
         initial="hidden"
         animate="visible"
-      ></motion.div>
+      ></motion.div> */}
       <LandingNav />
       <Hero />
       <ChatBot />
       <Footer />
-      <div className="ball fixed left-0 top-0 h-40 w-40 rounded-full bg-blue-500 blur-3xl"></div>
+      {/* <div className="ball fixed left-0 -z-[1] top-0 h-40 w-40 rounded-full bg-blue-500 blur-3xl"></div> */}
     </div>
   );
 }
