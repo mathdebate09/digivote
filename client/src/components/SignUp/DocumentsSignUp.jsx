8c1f8e152eae9b6aef4ac9117c0a5afca58bf597
Vivek <vivdave2004@gmail.com>
2024-09-28 11:03:59 +0530

    Rephrased in a better way

--- client/src/components/SignUp/DocumentsSignUp.jsx
+++ client/src/components/SignUp/DocumentsSignUp.jsx
@@ -1,10 +1,11 @@
-import React from "react";
-import {auth} from '../../firebase/config'
-import { useState } from "react";
+import React, { useState } from "react";
+
 import { useNavigate } from "react-router-dom";
 
-function DocumentsSignUp({setFormData, formData}) {
-  const [errorMessages,setErrorMessages] = useState(null);
+import { auth } from "../../firebase/config";
+
+function DocumentsSignUp({ setFormData, formData }) {
+  const [errorMessages, setErrorMessages] = useState(null);
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
@@ -17,56 +18,49 @@ function DocumentsSignUp({setFormData, formData}) {
       MedicalCertificate: data.get("MedCert"),
     };
 
-    if(!formData.name || !formData.password || !formData.email){
+    if (!formData.name || !formData.password || !formData.email) {
       return setErrorMessages("All fields are required");
     }
 
     try {
-
-      const res = await fetch('http://localhost:3000/api/auth/signup',{
+      const res = await fetch("http://localhost:3000/api/auth/signup", {
         method: "POST",
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(formData)
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
       });
       const data = await res.json();
 
-      if(data.success === false){
+      if (data.success === false) {
         setLoading(false);
         return setErrorMessages(data.message);
       }
 
-      setLoading(false)
-
-
-      if(res.ok){
-        navigate('/login')
-      }
-
-
-    } catch(error){
-      setErrorMessages(error.message);
       setLoading(false);
 
+      if (res.ok) {
+        navigate("/login");
+      }
+    } catch (error) {
+      setErrorMessages(error.message);
+      setLoading(false);
     }
-    
   };
 
-
   return (
-    <div className="flex h-screen -mt-20 flex-col items-center justify-center p-4 sm:p-6 md:p-8 lg:p-10">
-      <h1 className="mb-6 text-2xl font-bold text-center">
+    <div className="-mt-20 flex h-screen flex-col items-center justify-center p-4 sm:p-6 md:p-8 lg:p-10">
+      <h1 className="mb-6 text-center text-2xl font-bold">
         Finalise the submission with minimal Paperwork
       </h1>
       <form
         action="get"
         onSubmit={handleSubmit}
-        className="w-full max-w-md bg-white rounded-2xl border-2 border-black p-6 shadow-lg"
+        className="border-black w-full max-w-md rounded-2xl border-2 bg-white p-6 shadow-lg"
       >
         <label
           htmlFor="Aadhar"
           className="block p-1 text-lg font-medium text-gray-700"
         >
-          Enter your Aadhar
+          Submit your Aadhar
           <input
             type="file"
             name="Aadhar"
@@ -78,11 +72,10 @@ function DocumentsSignUp({setFormData, formData}) {
           htmlFor="Disability"
           className="block p-1 text-lg font-medium text-gray-700"
         >
-          Enter the disability (if any)
+          Please specify any accessibility features you may require.
           <select
             name="disability"
             id="disability"
-            
             className="mt-2 block w-full rounded-md border border-gray-300 bg-white px-4 py-2 shadow-sm transition duration-300 ease-in-out focus:border-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
           >
             <option value="default">Select</option>
